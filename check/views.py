beb50b57e0374be1be4edb9fb7fb3dfa28f5f006
manankshastri <manank.shastri@gmail.com>
2018-11-30 17:54:44 -0500

    added base.html

--- check/views.py
+++ check/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 
+
 # Create your views here.
 def index(request):
     return render(request, 'check/index.html')
