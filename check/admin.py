ddca549f3516d492d71abbb40243344dc7eb816d
manankshastri <manank.shastri@gmail.com>
2018-12-01 12:52:06 -0500

    exit

--- check/admin.py
+++ check/admin.py
@@ -2,15 +2,6 @@ from django.contrib import admin
 from .models import MProfile
 # Register your models here.
 
-"""
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('ssn', 'u', 'first_name', 'last_name')
-    fields = ['ssn', ('u', 'first_name', 'last_name'), 'gender']
-
-
-admin.site.register(Patient, PatientAdmin)
-"""
-
 
 @admin.register(MProfile)
 class ProfileAdmin(admin.ModelAdmin):
--- check/models.py
+++ check/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.urls import reverse
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
@@ -15,27 +14,6 @@ class MProfile(models.Model):
 
     def __str__(self):
         return self.user.username
-
-"""
-class Patient(models.Model):
-   #  for patient
-    GENDER_CHOICES = [('M', 'Male'), ('F', 'Female')]
-    u = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=False)
-    ssn = models.IntegerField('SSN', blank=False)
-    first_name = models.CharField('First Name', max_length=50)
-    last_name = models.CharField('Last Name', max_length=50)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-
-    class Meta:
-        ordering = ['first_name', 'last_name']
-
-    def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
-    def get_absolute_url(self):
-        return reverse('patient_detail', args=[str(self.ssn)])
-
-"""
 
 
 def create_profile(sender, **kwargs):
